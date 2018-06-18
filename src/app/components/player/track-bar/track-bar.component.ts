d864d2322a5ec8157e1960a9cd6cd8737eebc42f
Riccardo Gai <riccardog@lacunaclientscope.com>
2018-06-18 10:53:26 +0200

    style fixing, working on shuffle and random

--- src/app/components/player/track-bar/track-bar.component.ts
+++ src/app/components/player/track-bar/track-bar.component.ts
@@ -1,8 +1,12 @@
 import { Component, OnInit, Input } from '@angular/core'
 import { ISong } from '../../../models/ISong'
 import { CurrentSongService } from '../../../services/currentSongService/current-song.service'
-import {createSnackBar} from "../../../utilities/snackBar";
-
+import { createSnackBar } from '../../../utilities/snackBar'
+enum SongConfig {
+  CLASSIC = 0,
+  RANDOM = 1,
+  REPEAT = 2
+}
 @Component({
   selector: 'app-track-bar',
   templateUrl: './track-bar.component.html',
@@ -11,43 +15,54 @@ export class TrackBarComponent implements OnInit {
   constructor(private currentSongService: CurrentSongService) {}
   @Input() playlist: ISong[]
   public currentSong: ISong
+  public progress: number = 0
   public player = new Audio() //it returns a HTMLAudioElement
+  public songConfig: SongConfig = SongConfig
+  public configuration = SongConfig.CLASSIC
 
   ngOnInit() {
     this.currentSongService.getSong().subscribe((song: ISong) => {
       this.currentSong = song
       this.player.src = this.currentSong.src
-      this.play();
+      this.play()
     })
     this.player.volume = 0.5
-    this.player.addEventListener('error',  (e: any) =>  {
+    this.player.addEventListener('error', (e: any) => {
       switch (e.target.error.code) {
         case e.target.error.MEDIA_ERR_ABORTED:
-          createSnackBar('You aborted the video playback.')
-          break;
+          createSnackBar('You aborted the video playback.', true)
+          break
         case e.target.error.MEDIA_ERR_NETWORK:
-          createSnackBar('A network error caused the audio download to fail.')
-          break;
+          createSnackBar('A network error caused the audio download to fail.', true)
+          break
         case e.target.error.MEDIA_ERR_DECODE:
-          createSnackBar('The audio playback was aborted due to a corruption problem or because the video used features your browser did not support.')
-          break;
+          createSnackBar(
+            'The audio playback was aborted due to a corruption problem or because the video used features your browser did not support.', true
+          )
+          break
         case e.target.error.MEDIA_ERR_SRC_NOT_SUPPORTED:
-          createSnackBar('The video audio not be loaded, either because the server or network failed or because the format is not supported.')
-          break;
+          createSnackBar(
+            'The video audio not be loaded, either because the server or network failed or because the format is not supported.', true
+          )
+          break
         default:
-          createSnackBar('An unknown error occurred.');
-          break;
+          createSnackBar('An unknown error occurred.', true)
+          break
       }
     })
-    this.player.addEventListener('ended',() => {
-ì     this.goNext()
-    });
+    this.player.addEventListener('ended', () => {
+      this.goNext()
+    })
+    this.player.addEventListener('timeupdate', () => {
+      this.progress = this.player.currentTime
+    })
   }
   public play(): void {
     if (!this.currentSong) {
       this.currentSongService.setSong(this.playlist[0])
     } else {
-        this.player.play()
+      createSnackBar(`${this.currentSong.title}<br/>${this.currentSong.artist}`)
+      this.player.play()
     }
   }
   public pause(): void {
@@ -80,5 +95,17 @@ export class TrackBarComponent implements OnInit {
     if (this.existPrev()) {
       this.currentSongService.setSong(this.playlist[this.playlist.indexOf(this.currentSong) - 1])
     }
+  }
+  public secondsToMinute(seconds: number) {
+    seconds = seconds | 0
+    return (
+      Math.floor(seconds / 60) + '.' + (seconds % 60 ? (seconds % 60 < 10 ? '0' + (seconds % 60) : seconds % 60) : '00')
+    )
+  }
+  public setRepeat(): void {
+    this.configuration = this.configuration === SongConfig.REPEAT ? SongConfig.CLASSIC : SongConfig.REPEAT
+  }
+  public setRandom(): void {
+    this.configuration = this.configuration === SongConfig.RANDOM ? SongConfig.CLASSIC : SongConfig.RANDOM
   }
 }
--- src/app/utilities/snackBar.ts
+++ src/app/utilities/snackBar.ts
@@ -1,9 +1,14 @@
-export function createSnackBar(message: string) {
+export function createSnackBar(message: string, isError?: boolean) {
   const oldSnackBars = document.getElementById('snackbar')
   oldSnackBars && oldSnackBars.parentNode.removeChild(oldSnackBars)
   const snackbar = document.createElement('div')
   snackbar.setAttribute('id', 'snackbar')
-  snackbar.innerHTML = `<div class="align-items-center"><i class="material-icons red m-r-s">warning</i>${message}</div>`
+  if (isError) {
+    snackbar.innerHTML = `<div class="align-items-center"><i class="material-icons red m-r-s">warning</i>${message}</div>`
+  } else {
+    snackbar.innerHTML = `<div class="align-items-center"><i class="material-icons m-r-s">music_note</i>${message}</div>`
+  }
+
   document.body.appendChild(snackbar)
   setTimeout(() => {
     snackbar && snackbar.parentNode && snackbar.parentNode.removeChild(snackbar)
